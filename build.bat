d0830e78abd0832aa95ebbe03c1ac7db81816579
NewbieXvwu <87637612+NewbieXvwu@users.noreply.github.com>
2025-03-08 17:34:17 +0800

    Update for better perf

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-nvcc -O3 -arch=native --use_fast_math -Xcompiler "/O2 /fp:fast" hanoi_cuda.cu -o hanoi_cuda.exe
+nvcc -O3 -arch=native --use_fast_math -Xptxas "-O3" --threads 0 -Xcompiler "/O2,/fp:fast,/Ot,/Qpar" hanoi_cuda.cu -o hanoi_cuda.exe
 pause
